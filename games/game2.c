22fccfa379c86d3a94c34d4f90d54562048d651f
Teddy <97102706+teddyyeo@users.noreply.github.com>
2023-12-05 13:31:33 +0900

    [Intended Vulnerability Patch] Fixed BOF in game2
    removed dangerous strcpy and memcpy call

--- games/game2.c
+++ games/game2.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <time.h>
 
-char input[200] = {0,};
 
 void flag(int a, int b, int c, int d, int e, int f, int g)
 {
@@ -27,11 +26,7 @@ int main(void)
     srand(time(NULL));
 
     answer = rand() % 8 + 1;
-    fgets(input, 100, stdin);
-    if (input[23] == 'A')
-        memcpy(buffer, input, 199);
-    else
-        strcpy(buffer, input);
+    scanf("%3s", &buffer);
     guess = atoi(buffer) % 2;
     
     printf("%d\n", answer);
